0d438c522a31f59819546e4390572736891596bf
chmurny <Chmurny.Jaroslav-ext@komix.cz>
2026-01-28 22:12:00 +0100

    Message size reduced for eager_producer_tcp_client.py.

--- TCP-UDP/eager_producer_tcp_client.py
+++ TCP-UDP/eager_producer_tcp_client.py
@@ -87,8 +87,8 @@ def parse_cmd_line_args() -> Namespace:
 
 
 def generate_random_msg(client_name: str, seq_no: int) -> Dict[str, Any]:
-    number_count = randint(2000, 4000)
-    uuid_count = randint(2000, 4000)
+    number_count = randint(1000, 2000)
+    uuid_count = randint(1000, 2000)
     return {
         "client_name": client_name,
         "sequence_number": seq_no,
